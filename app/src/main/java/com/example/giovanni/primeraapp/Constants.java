d273a8049633c7bc15f01d543dcc7cc1df8e3303
Giovanni <giovanni.aravena.13@sansano.usm.cl>
2017-04-11 02:18:05 -0300

    Nueva Activity
    Pasar datos desde una activity a otra

---
+++ app/src/main/java/com/example/giovanni/primeraapp/Constants.java
@@ -0,0 +1,8 @@
+package com.example.giovanni.primeraapp;
+
+/**
+ * Created by giovanni on 04-04-17.
+ */
+
+public class Constants {
+}
---
+++ app/src/main/java/com/example/giovanni/primeraapp/SaludarActivity.java
@@ -0,0 +1,13 @@
+package com.example.giovanni.primeraapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SaludarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_saludar);
+    }
+}
